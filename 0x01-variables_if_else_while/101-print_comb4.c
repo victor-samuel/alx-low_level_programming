b9563b54d9fb0671e33e5e902f886bfa9c29915f
VicTM <kingbrovickey@gmail.com>
2022-10-25 21:33:31 +0300

    print-comb3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,8 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				if (!((ones == tens) || (tens == hundreds)
+						       	|| (tens > ones) || (hundreds > tens)))
 				{
 					putchar(hundreds);
 					putchar (tens);
