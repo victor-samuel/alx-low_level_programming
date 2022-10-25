471b3e88ba2bfa623ff6caf48108e1e71f97adca
VicTM <kingbrovickey@gmail.com>
2022-10-25 20:32:32 +0300

    print-comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,6 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	
 	for (tens = '0'; tens <= '9'; tens++) /* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
