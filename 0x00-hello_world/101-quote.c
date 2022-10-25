d1aef3f26e35613eea0ca0402902d113bfc3d9f5
VicTM <kingbrovickey@gmail.com>
2022-10-25 14:07:35 +0300

    intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  * Return: Always 0 (Success)
  */
 
-int main(void){
-	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
