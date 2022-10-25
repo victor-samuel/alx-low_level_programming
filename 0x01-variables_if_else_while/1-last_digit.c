6682a807cec0e97a63cf5e530b223215b3d7cd6a
VicTM <kingbrovickey@gmail.com>
2022-10-25 17:26:40 +0300

    last-digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-	printf("last digit of %d is", n);
+	printf("last digit of %d is ", n);
 	num = n % 10;
         if (num > 5)
         {
