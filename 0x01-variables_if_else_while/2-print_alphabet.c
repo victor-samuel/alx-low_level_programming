ae0518cf468d8d6650116d32478f701aa007d454
VicTM <kingbrovickey@gmail.com>
2022-10-25 17:40:33 +0300

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while(ch == '2')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+}
