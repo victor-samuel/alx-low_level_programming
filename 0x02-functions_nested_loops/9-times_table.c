c56bf14889db7a516d466ef3f3c2efbbb8e33496
VicTM <kingbrovickey@gmail.com>
2022-10-26 17:48:30 +0300

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if ( i < 9 && j < 9)
+			if ( i <= 9 && j <= 9)
 			{
 				_putchar(((i * j) / 10) + '0');
 				_putchar(((i * j) % 10) + '0');
