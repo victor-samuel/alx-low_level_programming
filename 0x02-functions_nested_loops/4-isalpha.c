c2b7d2a057dcf9c378fe087246a70f9b02271bec
VicTM <kingbrovickey@gmail.com>
2022-10-26 09:09:36 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - checks if its a lowercase or uppercase
+ * @c: character argument
+ * Return: return 1 or 0 depending on the alphabet
+ */
+int _isalpha(int c);
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+}
