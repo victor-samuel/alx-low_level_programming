3cbdcbdbf41167906642ceec34d14a45fced07d4
VicTM <kingbrovickey@gmail.com>
2022-10-26 08:55:31 +0300

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - print all lowercase alphabets ten times
+ * print_alphabet_x10 - print all lowercase alphabets ten times
  */
 
 void print_alphabet_x10(void)
