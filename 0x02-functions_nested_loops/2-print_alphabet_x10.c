7db389d43557d8af5b0d44357b79c4b32b6a1ad5
VicTM <kingbrovickey@gmail.com>
2022-10-26 08:31:24 +0300

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+i#include "main.h"
+/**
+ * main - Entry point
+ * print_alphabet_x10 -> prints the lowercase letters
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+        int j;
+	int i;
+
+        for (i = 0; i < 10; i++)
+        {
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
+
