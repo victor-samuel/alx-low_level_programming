0ca5f941e4c72e3c59629770bcbf39f8c66513bf
VicTM <kingbrovickey@gmail.com>
2022-10-26 17:07:25 +0300

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ */
+void times_table(void)
+{
+	int i, j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar((i * j) + '0');
+			_putchar(',');
+			_putchar('.');
+			_putchar('.');
+		}
+		_putchar('\n');
+		_putchar('$');
+	}
+}
+
