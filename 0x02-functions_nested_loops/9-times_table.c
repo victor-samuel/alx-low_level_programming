151569355ec1eeaa09ea4de2ac1b8702be0b91ee
VicTM <kingbrovickey@gmail.com>
2022-10-26 19:27:27 +0300

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,28 +1,29 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
 /**
  *times_table - prints the multiplication table
  */
 void times_table(void)
 {
 	int i, j;
-	for (i = 0; i < 10; i++)
+	int mat [SIZE] [SIZE];
+
+	for (i = 0; i < SIZE; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < SIZE; j++)
 		{
-			if ( i <= 8 && j <= 8)
-			{
-				_putchar(((i * j) / 10) + '0');
-				_putchar(((i * j) % 10) + '0');
-				_putchar(',');
-			}
-			else if (j >= 8)
-			{
-			_putchar(((i * j) / 10) + '0');
-			_putchar(((i * j) % 10) + '0');
-			_putchar('\n');
-			}
+			mat [i] [j]= i * j;
 		}
 	}
-}
 
+	for (i = 0; i < SIZE; i++)
+        {       
+                for (j = 0; j < SIZE; j++)
+                {       
+                        printf("%d" ,mat [i] [j]);
+                }
+		print('\n');
+        }
+}
