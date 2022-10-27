7a339b848d1e529cc574ada17257a03d55b60a22
VicTM <kingbrovickey@gmail.com>
2022-10-27 13:04:33 +0300

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest_number(int a, int b, int c)
 
     if (a > b && a > c)
     {
-        largest = a;
+	    largest = a;
     }
     else if (b > c && b > a)
     {
-        largest = b;
+	    largest = b;
     }
     else if (c > b)
     {
-        largest = c;
+	    largest = c;
     }
     else
     {
