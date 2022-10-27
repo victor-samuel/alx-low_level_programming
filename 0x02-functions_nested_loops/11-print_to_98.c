5275f868243f371fed01b027337a9d579c15cbbe
VicTM <kingbrovickey@gmail.com>
2022-10-27 11:48:32 +0300

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_to_98 - prints from 0 to 98
+ * @n: integer to be printed
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+	printf("%d\n", n);
+	}
+	return (n);
+}	
